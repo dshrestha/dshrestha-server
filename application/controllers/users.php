64e13e36303c934ea02bc7d8abae1ed3a836ece3
Deewendra Shrestha <dshrestha@affinnova.com>
2014-10-10 12:26:36 -0400

    WIP : login

---
+++ application/controllers/users.php
@@ -0,0 +1,39 @@
+<?php
+class Users extends CI_Controller{
+	
+	public function __construct()
+	{
+	    parent::__construct();
+	    
+	    /* Load the libraries and helpers */
+	    $this->load->library(array("session","form_validation"));
+	    $this->load->helper(array('url','date'));
+	    $this->load->database();	    
+        $this->load->model('User');
+	}
+
+	public function authenticate(){
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+		
+		if ($this->form_validation->run() == FALSE) {
+			$errors = array();
+			$this->form_validation->set_error_delimiters('','');
+			if($this->form_validation->error('username')!="") {
+				array_push($errors,array('field'=>'username','message'=>array($this->form_validation->error('username'))));
+			}
+			if($this->form_validation->error('password')!=""){
+				array_push($errors,array('field'=>'password','message'=>array($this->form_validation->error('password'))));
+			}
+
+			http_response_code(422);
+			header('Content-type: application/json');
+			echo (json_encode(array("errors"=>$errors)));				
+		}
+		else {
+			echo 'success';
+			
+		}
+	}
+}
+?>
---
+++ application/models/user.php
@@ -0,0 +1,10 @@
+<?php
+class User extends CI_Model {
+
+    var $id             = null;
+    var $username     	= null;
+    var $password     	= null;
+    var $role    		= null;
+    
+}
+?>
